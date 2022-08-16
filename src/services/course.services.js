b0ac9e16437bfefd00179465f010e47c86abcd5f
devumairazmat <umairazmat8260@gmail.com>
2022-08-17 02:10:14 +0500

    update service added

--- src/services/course.services.js
+++ src/services/course.services.js
@@ -1,23 +1,29 @@
-import {db} from "../firebase-config";
+import { db } from "../firebase-config";
 import {
-    collection,
-    getDoc,
-    getDocs,
-    addDoc,
-    updateDoc,
-    deleteDoc,
-    doc
+  collection,
+  getDoc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
 } from "./firebase-services";
 
-const courseCollectionRef = collection(db,"courses")
+const courseCollectionRef = collection(db, "courses");
 
 class CourseDataService {
-  
-    // Add new Course
-    addCourses = (newCourse) => {
-        return addDoc(courseCollectionRef,newCourse)
-    }
+  // Add new Course
+  addCourses = (newCourse) => {
+    return addDoc(courseCollectionRef, newCourse);
+  };
 
+  // Update Course
+  updateCourse = (id, updateCourse) => {
+    const courseDoc = doc(db, "courses", id);
+    return updateDoc(courseDoc, updateCourse);
+  };
+
+  
 }
 
 export default new CourseDataService();
