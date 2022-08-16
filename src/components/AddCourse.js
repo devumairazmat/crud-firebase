712b3366b85fbfe741029cb3f99a6213906b7bf2
devumairazmat <umairazmat8260@gmail.com>
2022-08-17 04:02:36 +0500

    add feature done

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -1,31 +1,94 @@
 import React from "react";
 import { useState } from "react";
 import { Form, Alert, InputGroup, Button, ButtonGroup } from "react-bootstrap";
+import CourseDataService from "../services/course.services"
 
 function AddCourse() {
+  const [name, setName] = useState("");
+  const [trainer, setTrainer] = useState("");
   const [status, setStatus] = useState("Available");
   const [flag, setFlag] = useState(true);
+  const [message, setMessage] = useState({error:false, msg: "" });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    if (name === "" || trainer === "") {
+      setMessage({error:true, msg: "Please fill all the fields" });
+      return;
+    }
+    const newCourse = {
+      name,
+      trainer,
+      status
+    };
+    console.log(newCourse);
+    try {
+     await CourseDataService.addCourses(newCourse);
+     setMessage({error:false, msg: "Course added successfully" });
+    }
+    catch (error) {
+      setMessage({error:true, msg: "Error adding course" });
+    }
+    setName("");
+    setTrainer("");
+  }
+
   return (
     <>
       <div className="p-4 box">
-        <Form>
+        {message?.msg && (
+          <Alert
+            variant={message?.error ? "danger" : "success"}
+            dismissible
+            onClose={() => setMessage("")}
+          >
+            {message?.msg}
+          </Alert>
+        )}
+        
+        <Form onSubmit={handleSubmit} >
           <Form.Group className="mb-3" controlId="formCourseTitle">
             <InputGroup>
               <InputGroup.Text id="formCourseTitle">Course</InputGroup.Text>
-              <Form.Control type="text" placeholder="Course Name" />
+              <Form.Control
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                placeholder="Course Name"
+              />
             </InputGroup>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formCourseTrainer">
             <InputGroup>
               <InputGroup.Text id="formCourseTrainer">Trainer</InputGroup.Text>
-              <Form.Control type="text" placeholder="Course Trainer" />
+              <Form.Control
+                value={trainer}
+                onChange={(e) => setTrainer(e.target.value)}
+                type="text"
+                placeholder="Course Trainer"
+              />
             </InputGroup>
           </Form.Group>
           <ButtonGroup aria-label="Basic example" className="mb-3">
-            <Button  variant="success">
+          <Button
+              disabled={flag}
+              variant="success"
+              onClick={(e) => {
+                setStatus("Available");
+                setFlag(true);
+              }}
+            >
               Available
             </Button>
-            <Button variant="danger" >
+            <Button
+              variant="danger"
+              disabled={!flag}
+              onClick={(e) => {
+                setStatus("Not Available");
+                setFlag(false);
+              }}
+            >
               Not Available
             </Button>
           </ButtonGroup>
