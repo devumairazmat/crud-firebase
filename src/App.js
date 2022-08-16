52dbe76037f9cb6c1ae38875bfaf7185a739e10d
devumairazmat <umairazmat8260@gmail.com>
2022-08-17 02:23:52 +0500

    Navbar added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { Container, Navbar, Row, Col } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from 'react-bootstrap';
-import './App.css';
+import "./App.css";
+import AddCourse from "./components/AddCourse";
+import CourseList from "./components/CourseList";
 
 function App() {
   return (
   <>
-  <Container>
-    <h1>Hello World</h1>
-  </Container>
+   <Navbar bg="dark" variant="dark" className="header">
+        <Container>
+          <Navbar.Brand href="#home">Library - Firebase CRUD</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container style={{ width: "400px" }}>
+        <Row>
+          <Col>
+            <AddCourse />
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row>
+          <Col>
+            <CourseList  />
+          </Col>
+        </Row>
+      </Container>
   </>
   );
 }
