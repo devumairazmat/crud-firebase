3a06729c95a49eef9bc277dac56bec3da60261b7
devumairazmat <umairazmat8260@gmail.com>
2022-08-17 03:11:59 +0500

    course list added

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Table, Button } from "react-bootstrap";
+
 
 function CourseList() {
   return (
-    <div>CourseList</div>
-  )
+    <>
+      <div className="mb-2">
+        <Button variant="dark edit">Refresh List</Button>
+      </div>
+
+      {/* <pre>{JSON.stringify(books, undefined, 2)}</pre>} */}
+      <Table striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Course Name</th>
+            <th>Trainer Name</th>
+            <th>Status</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+              <tr>
+                <td>1</td>
+                <td>Moile App Dev</td>
+                <td>Uzair</td>
+                <td>Available</td>
+                <td>
+                  <Button variant="secondary" className="edit">
+                    Edit
+                  </Button>
+                  <Button variant="danger" className="delete">
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 }
 
-export default CourseList
+export default CourseList;
