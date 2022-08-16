356951963449be9a95dd9528c728704b69833743
devumairazmat <umairazmat8260@gmail.com>
2022-08-17 02:54:35 +0500

    add course Design added

--- src/components/AddCourse.js
+++ src/components/AddCourse.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Form, Alert, InputGroup, Button, ButtonGroup } from "react-bootstrap";
 
 function AddCourse() {
+  const [status, setStatus] = useState("Available");
+  const [flag, setFlag] = useState(true);
   return (
-    <div>AddCourse</div>
-  )
+    <>
+      <div className="p-4 box">
+        <Form>
+          <Form.Group className="mb-3" controlId="formCourseTitle">
+            <InputGroup>
+              <InputGroup.Text id="formCourseTitle">Course</InputGroup.Text>
+              <Form.Control type="text" placeholder="Course Name" />
+            </InputGroup>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formCourseTrainer">
+            <InputGroup>
+              <InputGroup.Text id="formCourseTrainer">Trainer</InputGroup.Text>
+              <Form.Control type="text" placeholder="Course Trainer" />
+            </InputGroup>
+          </Form.Group>
+          <ButtonGroup aria-label="Basic example" className="mb-3">
+            <Button  variant="success">
+              Available
+            </Button>
+            <Button variant="danger" >
+              Not Available
+            </Button>
+          </ButtonGroup>
+          <div className="d-grid gap-2">
+            <Button variant="primary" type="Submit">
+              Add/ Update
+            </Button>
+          </div>
+        </Form>
+      </div>
+    </>
+  );
 }
 
-export default AddCourse
+export default AddCourse;
